e98ee91b9fac17d123aa560bd721808a87c1c8dd
Joao Gomes <jmarcosfmg@gmail.com>
2022-10-16 16:41:40 -0300

    refactor: :wastebasket: Code cleaning
    Removed unused imports and logs

--- src/num_adder.js
+++ src/num_adder.js
@@ -1,11 +1,7 @@
-const input = process.argv
-
 module.exports = {
     add: addnums
 }
 
 function addnums(num1, num2){
-    let result = parseInt(num1) + parseInt(num2)
-    console.log(result)
-    return result
+    return parseInt(num1) + parseInt(num2)
 }
